ad26e1bf2b08d5680f532e472d5a1b2e04fc8b5d
AndrzejZielnski <andrzej.zielinski@perfectsystem.pl>
2016-01-19 22:05:41 +0100

    Dodanie wykrywania zielonego koloru

--- proj.py
+++ proj.py
@@ -1,22 +1,36 @@
 import numpy as np
 import cv2
 
-## Definiujemy obiekt z indeksem urządzenia - w moim przypadku domyslne urzadzenie
+## Definiujemy obiekt z indeksem urzadzeienia - w moim przypadku domyslne urzadzenie
 cap = cv2.VideoCapture(0)
 
 while(True):
     # Przechwytywanie klatek
-    ret, frame = cap.read()
+	ret, frame = cap.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+# Konwertujemy RGB TO HSV
 
-## Wyswietlamy wynik
+	new = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):   ## ustawienie uczekiwania i przerwania na klawisz "q"
-        break
+## Definiujemy zakres koloru w HSV
 
-# Zwalniamy przechwytywanie i zabijamy okno
-cap.release()
+	min = np.array([40, 100, 50], dtype=np.uint8)
+	max = np.array([80, 255, 255], dtype=np.uint8)
+
+## Ograniczenie do pokazania tylko zielonego koloru
+
+	mask = cv2.inRange(new, min, max)
+
+## Obiekt - iloczyn maski i oryginalnego brazu
+
+	res = cv2.bitwise_and(frame,frame, mask= mask)
+
+
+	cv2.imshow('frame',frame)
+   	cv2.imshow('mask',mask)
+    	cv2.imshow('res',res)
+    	exit = cv2.waitKey(5) & 0xFF
+    	if exit == 27:    ## kod przycisku wyjscia
+        	break
+
 cv2.destroyAllWindows()
